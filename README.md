95c4cf1e9a002cd835552e0735bbddba317c8eb8
Sebastian Czoch <sebastian@czoch.pl>
2018-08-21 21:40:16 +0200

    Add first version of script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # telegraf-cloudflare
 Plugin for Telegraf for gathering statistics from Cloudflare
+
+This script is in beta version, use with caution
+
+## Installation
+```
+$ pip install telegraf-cloudflare
+```
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        return f.read()
+
+
+setuptools.setup(name='telegraf-cloudflare',
+                 description='Plugin for Telegraf for gathering statistics from Cloudflare',
+                 long_description=long_description,
+                 version='0.1.0',
+                 url='https://github.com/SebastianCzoch/telegraf-cloudflare',
+                 author='Sebastian Czoch',
+                 author_email='sebastian@czoch.pl',
+                 license='MIT',
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: System Administrators',
+                     'License :: OSI Approved :: MIT License'
+                     'Programming Language :: Python :: 2'
+                     'Operating System :: OS Independent',
+                 ],
+                 packages=setuptools.find_packages(),
+                 install_requires=read_file('requirements.txt').splitlines(),
+                 scripts=['telegraf-cloudflare']
+                 )
